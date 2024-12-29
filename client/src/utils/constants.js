17d3a0550ac3017d6e961b4627b9140964a6bc91
Masoud <masoud_mdar@yahoo.fr>
2024-12-29 15:59:32 +0100

    server address updated

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,3 +1,3 @@
 // export default BASE_URL = "https://exercise-tracker-msd.herokuapp.com";
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = "https://exercise-tracker-hglq.onrender.com";
 export default BASE_URL
